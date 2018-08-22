c0c403bace0c90c9928cfd224321d8b5fdd7dcef
Achsah Abraham <achsahmp@gmail.com>
2018-08-22 20:53:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,15 +5,15 @@ class Dog
 
   def initialize(name)
     @name = name
-  
+
   end
 
   def self.clear_all
   end
 
   def self.all
-    @@all.each do |dog|
-      puts dog.name
+    @@all.each do |dog_name|
+      puts dog_name.name
     end
   end
 end
