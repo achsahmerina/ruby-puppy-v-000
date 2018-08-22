43e14710dc9ca2c40e8a69fae4ac2282ee88b2d9
Achsah Abraham <achsahmp@gmail.com>
2018-08-22 21:20:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,18 @@
 class Dog
-  @@all = []
-
   attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
-
+    @@all << self
   end
 
   def self.clear_all
   end
 
   def self.all
-    @@all.each do |dog_name|
-      puts dog_name.name
+    @@all.each do |dog|
+      puts dog.name
     end
 
   end
